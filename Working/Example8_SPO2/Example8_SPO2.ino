4d148732860666290897724bce8cf5985a871013
llyando <llyando@users.noreply.github.com>
2021-02-19 22:40:39 -0500

    Added temp output to serial print output

--- Working/Example8_SPO2/Example8_SPO2.ino
+++ Working/Example8_SPO2/Example8_SPO2.ino
@@ -27,9 +27,16 @@
 */
 
 #include <Wire.h>
+#include <Adafruit_MLX90614.h>
+
 #include "MAX30105.h"
 #include "spo2_algorithm.h"
 
+
+// Temp sensor objects
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+// PulseOx sensor objects
 MAX30105 particleSensor;
 
 #define MAX_BRIGHTNESS 255
@@ -56,20 +63,33 @@ byte readLED = 13; //Blinks with each data read
 void setup()
 {
   Serial.begin(115200); // initialize serial communication at 115200 bits per second:
+  
+  // Start temp sensor
+  mlx.begin();  
 
+  // PulseOx Leds
   pinMode(pulseLED, OUTPUT);
   pinMode(readLED, OUTPUT);
 
-  // Initialize sensor
+  // Initialize PulseOx sensor
+  particleSensor.begin(Wire, I2C_SPEED_FAST);
+  
+/* Removed check for PulseOx sensor 
   if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
   {
     Serial.println(F("MAX30105 was not found. Please check wiring/power."));
     while (1);
   }
+*/
 
-//  Serial.println(F("Attach sensor to finger with rubber band. Press any key to start conversion"));
-//  while (Serial.available() == 0) ; //wait until user presses a key
-  Serial.read();
+/*  
+  PulseOx code asking for confirmation before beginning has been turned off
+  Serial.println(F("Attach sensor to finger with rubber band. Press any key to start conversion"));
+  while (Serial.available() == 0); //wait until user presses a key 
+  {
+    Serial.read();
+  }
+*/
 
   byte ledBrightness = 60; //Options: 0=Off to 255=50mA
   byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
@@ -83,6 +103,9 @@ void setup()
 
 void loop()
 {
+	
+	
+  // ------------------------ PulseOx sensor code START ------------------------
   bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
 
   //read the first 100 samples, and determine the signal range
@@ -143,10 +166,16 @@ void loop()
 
       Serial.print(F(", SPO2Valid="));
       Serial.println(validSPO2, DEC);
+
+    // Added Temp sensor to print out. Not ideal but serves for now at least.
+	  Serial.print(F(", TEMP="));
+	  Serial.println( mlx.readObjectTempF() );
     }
 
     //After gathering 25 new samples recalculate HR and SP02
     maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
   }
+  
+  // ------------------------ PulseOx sensor code END ------------------------
   
 }
