d056e2b70ca9334d4ac2a02ab9376ce408e7f415
llyando <llyando@users.noreply.github.com>
2021-03-03 11:36:09 -0500

    Removed pusle ox activity check. Need to convert serial code to ROS publish code.

--- Working/i2c_testing/i2c_testing.ino
+++ Working/i2c_testing/i2c_testing.ino
@@ -38,9 +38,9 @@ void setup()
 {
   Serial.begin(115200);
   Wire.begin();
-  
+
   // Start temp sensor
-  mlx.begin();  
+  mlx.begin();
 
   // Initialize PulseOx sensor
   particleSensor.begin(Wire);
@@ -49,13 +49,13 @@ void setup()
   pinMode(pulseLED, OUTPUT);
   pinMode(readLED, OUTPUT);
 
-/* Removed check for PulseOx sensor 
-  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
-  {
-    Serial.println(F("MAX30105 was not found. Please check wiring/power."));
-    while (1);
-  }
-*/
+  /* Removed check for PulseOx sensor
+    if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+    {
+      Serial.println(F("MAX30105 was not found. Please check wiring/power."));
+      while (1);
+    }
+  */
 
   byte ledBrightness = 60; //Options: 0=Off to 255=50mA
   byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
@@ -69,24 +69,24 @@ void setup()
 
 void loop()
 {
-  
+
   // ------------------------ PulseOx sensor code START ------------------------
   bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
 
   //read the first 100 samples, and determine the signal range
   for (byte i = 0 ; i < bufferLength ; i++)
   {
-    while (particleSensor.available() == false) //do we have new data?
-      particleSensor.check(); //Check the sensor for new data
+    //    while (particleSensor.available() == false) //do we have new data?
+    //      particleSensor.check(); //Check the sensor for new data
 
     redBuffer[i] = particleSensor.getRed();
     irBuffer[i] = particleSensor.getIR();
     particleSensor.nextSample(); //We're finished with this sample so move to next sample
 
-//    Serial.print(F("red="));
-//    Serial.print(redBuffer[i], DEC);
-//    Serial.print(F(", ir="));
-//    Serial.println(irBuffer[i], DEC);
+    //    Serial.print(F("red="));
+    //    Serial.print(redBuffer[i], DEC);
+    //    Serial.print(F(", ir="));
+    //    Serial.println(irBuffer[i], DEC);
   }
 
   //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
@@ -96,16 +96,16 @@ void loop()
   while (1)
   {
 
-   Serial.print(", TEMP=");
-   Serial.println(mlx.readObjectTempF());
+    Serial.print(", TEMP=");
+    Serial.println(mlx.readObjectTempF());
 
-   Serial.print(F(", HR="));
-   Serial.println(heartRate, DEC);
+    Serial.print(F(", HR="));
+    Serial.println(heartRate, DEC);
 
-   Serial.print(F(", SPO2="));
-   Serial.println(spo2, DEC);
+    Serial.print(F(", SPO2="));
+    Serial.println(spo2, DEC);
 
-    
+
     //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
     for (byte i = 25; i < 100; i++)
     {
@@ -116,11 +116,11 @@ void loop()
     //take 25 sets of samples before calculating the heart rate.
     for (byte i = 75; i < 100; i++)
     {
-     /*
-      while (particleSensor.available() == false) //do we have new data?
-        particleSensor.check(); //Check the sensor for new data
-     */
-     
+      /*
+        while (particleSensor.available() == false) //do we have new data?
+         particleSensor.check(); //Check the sensor for new data
+      */
+
       digitalWrite(readLED, !digitalRead(readLED)); //Blink onboard LED with every data read
 
       redBuffer[i] = particleSensor.getRed();
@@ -128,30 +128,30 @@ void loop()
       particleSensor.nextSample(); //We're finished with this sample so move to next sample
 
       //send samples and calculation result to terminal program through UART
-//      Serial.print(F("red="));
-//      Serial.print(redBuffer[i], DEC);
-//      Serial.print(F(", ir="));
-//      Serial.print(irBuffer[i], DEC);
-//
-//      Serial.print(F(", HR="));
-//      Serial.print(heartRate, DEC);
-//
-//      Serial.print(F(", HRvalid="));
-//      Serial.print(validHeartRate, DEC);
-//
-//      Serial.print(F(", SPO2="));
-//      Serial.print(spo2, DEC);
-//
-//      Serial.print(F(", SPO2Valid="));
-//      Serial.println(validSPO2, DEC);
-//
-     
+      //      Serial.print(F("red="));
+      //      Serial.print(redBuffer[i], DEC);
+      //      Serial.print(F(", ir="));
+      //      Serial.print(irBuffer[i], DEC);
+      //
+//            Serial.print(F(", HR="));
+//            Serial.print(heartRate, DEC);
+      //
+      //      Serial.print(F(", HRvalid="));
+      //      Serial.print(validHeartRate, DEC);
+      //
+//            Serial.print(F(", SPO2="));
+//            Serial.print(spo2, DEC);
+      //
+//            Serial.print(F(", SPO2Valid="));
+//            Serial.println(validSPO2, DEC);
+      //
+
     }
 
     //After gathering 25 new samples recalculate HR and SP02
     maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
   }
-  
+
   // ------------------------ PulseOx sensor code END ------------------------
-  
+
 }
